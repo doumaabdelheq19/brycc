9dfe37cd8aa456de0afeaf12a7f88f1023824a07
doumaabdelheq19 <104992323+doumaabdelheq19@users.noreply.github.com>
2023-09-16 11:12:42 +0100

    Update papaer.blade.php

--- resources/views/categories/papaer.blade.php
+++ resources/views/categories/papaer.blade.php
@@ -342,7 +342,7 @@ if (!event.target.matches('.dropbtn')) {
             ->where('category', $course_id)
             ->get();?>
         @foreach ($cods as $cod)
-          <a class="box" href="{{route('card.edit', $cod->id)}}">
+          <a class="box" href="{{url('papca' , $cod->id)}}">
             <div class="image">
                 <img src="{{asset('/storage/'.$cod->attachement) }}" alt="">
             </div>
